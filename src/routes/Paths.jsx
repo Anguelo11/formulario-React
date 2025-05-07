695a36878bcf74f66d1f31bc264f26cc9531ee95
angelo <angeloguilherme580@gmail.com>
2025-05-07 17:12:24 -0300

    testando outra solucao

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -5,7 +5,11 @@ import Feedback from '../components/FeedbackForm';
 const Paths = () => {
     return(
         <>
-           <BrowserRouter basename="/formulario-React"></BrowserRouter>
+            <BrowserRouter basename="/formulario-React">
+                <Routes>
+                    <Route path="/" element={<Feedback/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
